c7ff5eb383d3bb865406835c262acbb4f6fdcf23
Liam Ryan <128687090+LiamRyan1@users.noreply.github.com>
2024-11-10 20:47:07 +0000

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 //port connecting to
 const port = 4000;
+//import and enable cors
 const cors = require('cors');
 app.use(cors());
 
+//enable use of the following headers
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+//import bodyparser 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
